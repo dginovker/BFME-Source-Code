b2762594abc25f3e17accb91f95bfdf55c052dec
dginovker <danielginovker@gmail.com>
2026-06-28 01:57:06 -0700

    Match Coord3D arithmetic functions

--- src/math/coord3d.cpp
+++ src/math/coord3d.cpp
@@ -67,6 +67,145 @@ Coord3D &Coord3D::operator=(const Coord3DBase &that)
     return *this;
 }
 
+float Coord3D::operator*(const Coord2D &that) const
+{
+    return x * that.x + y * that.y;
+}
+
+float Coord3D::operator*(const Coord3DBase &that) const
+{
+    return x * that.x + y * that.y + z * that.z;
+}
+
+Coord3D &Coord3D::operator*=(float scale)
+{
+    x *= scale;
+    y *= scale;
+    z *= scale;
+    return *this;
+}
+
+Coord3D &Coord3D::operator+=(const Coord2D &that)
+{
+    x += that.x;
+    y += that.y;
+    return *this;
+}
+
+Coord3D &Coord3D::operator+=(const Coord3DBase &that)
+{
+    x += that.x;
+    y += that.y;
+    z += that.z;
+    return *this;
+}
+
+Coord3D &Coord3D::operator-=(const Coord2D &that)
+{
+    x -= that.x;
+    y -= that.y;
+    return *this;
+}
+
+Coord3D &Coord3D::operator-=(const Coord3DBase &that)
+{
+    x -= that.x;
+    y -= that.y;
+    z -= that.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Add(const Coord2D &that)
+{
+    x += that.x;
+    y += that.y;
+    return *this;
+}
+
+Coord3D &Coord3D::Add(const Coord2D &left, const Coord3DBase &right)
+{
+    x = left.x + right.x;
+    y = left.y + right.y;
+    z = right.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Add(const Coord3DBase &that)
+{
+    x += that.x;
+    y += that.y;
+    z += that.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Add(const Coord3DBase &left, const Coord2D &right)
+{
+    x = left.x + right.x;
+    y = left.y + right.y;
+    z = left.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Add(const Coord3DBase &left, const Coord3DBase &right)
+{
+    x = left.x + right.x;
+    y = left.y + right.y;
+    z = left.z + right.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Add2D(const Coord3DBase &that)
+{
+    x += that.x;
+    y += that.y;
+    return *this;
+}
+
+Coord3D &Coord3D::Add2D(const Coord3DBase &left, const Coord3DBase &right)
+{
+    x = left.x + right.x;
+    y = left.y + right.y;
+    return *this;
+}
+
+Coord3D &Coord3D::Negate()
+{
+    x = -x;
+    y = -y;
+    z = -z;
+    return *this;
+}
+
+Coord3D &Coord3D::Scale(float scale)
+{
+    x *= scale;
+    y *= scale;
+    z *= scale;
+    return *this;
+}
+
+Coord3D &Coord3D::Scale(const Coord3DBase &that, float scale)
+{
+    x = that.x * scale;
+    y = that.y * scale;
+    z = that.z * scale;
+    return *this;
+}
+
+Coord3D &Coord3D::Scale2D(float scale)
+{
+    x *= scale;
+    y *= scale;
+    return *this;
+}
+
+Coord3D &Coord3D::Scale2D(const Coord3DBase &that, float scale)
+{
+    x = that.x * scale;
+    y = that.y * scale;
+    return *this;
+}
+
 Coord3D &Coord3D::Set(float x, float y, float z)
 {
     this->x = x;
@@ -131,6 +270,20 @@ void Coord3D::zero()
     ((unsigned int *)this)[2] = 0;
 }
 
+void Coord3D::add(const Coord3DBase *that)
+{
+    x += that->x;
+    y += that->y;
+    z += that->z;
+}
+
+void Coord3D::scale(float scale)
+{
+    x *= scale;
+    y *= scale;
+    z *= scale;
+}
+
 void Coord3D::set(float x, float y, float z)
 {
     this->x = x;
@@ -145,10 +298,70 @@ void Coord3D::set(const Coord3DBase *that)
     z = that->z;
 }
 
+void Coord3D::sub(const Coord3DBase *that)
+{
+    x -= that->x;
+    y -= that->y;
+    z -= that->z;
+}
+
 Coord3D &Coord3D::SetZero()
 {
     ((unsigned int *)this)[2] = 0;
     ((unsigned int *)this)[1] = 0;
     ((unsigned int *)this)[0] = 0;
+    return *this;
+}
+
+Coord3D &Coord3D::Sub(const Coord2D &that)
+{
+    x -= that.x;
+    y -= that.y;
+    return *this;
+}
+
+Coord3D &Coord3D::Sub(const Coord2D &left, const Coord3DBase &right)
+{
+    x = left.x - right.x;
+    y = left.y - right.y;
+    z = -right.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Sub(const Coord3DBase &that)
+{
+    x -= that.x;
+    y -= that.y;
+    z -= that.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Sub(const Coord3DBase &left, const Coord2D &right)
+{
+    x = left.x - right.x;
+    y = left.y - right.y;
+    z = left.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Sub(const Coord3DBase &left, const Coord3DBase &right)
+{
+    x = left.x - right.x;
+    y = left.y - right.y;
+    z = left.z - right.z;
+    return *this;
+}
+
+Coord3D &Coord3D::Sub2D(const Coord3DBase &that)
+{
+    x -= that.x;
+    y -= that.y;
+    return *this;
+}
+
+Coord3D &Coord3D::Sub2D(const Coord3DBase &left, const Coord3DBase &right)
+{
+    x = left.x - right.x;
+    y = left.y - right.y;
     return *this;
 }
