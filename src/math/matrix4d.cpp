217341a7b65287ccd5f7a83080aac2c8461d2c53
dginovker <danielginovker@gmail.com>
2026-06-28 02:15:31 -0700

    Match Matrix4D identity functions

--- src/math/matrix4d.cpp
+++ src/math/matrix4d.cpp
@@ -1,5 +1,35 @@
 #include "matrix4d.h"
 
+Matrix4D::Matrix4D()
+{
+}
+
+Matrix4D::Matrix4D(bool identity)
+{
+    unsigned int zero = 0;
+
+    if (identity) {
+        unsigned int one = 0x3f800000;
+
+        ((unsigned int *)values)[0] = one;
+        ((unsigned int *)values)[1] = zero;
+        ((unsigned int *)values)[2] = zero;
+        ((unsigned int *)values)[3] = zero;
+        ((unsigned int *)values)[4] = zero;
+        ((unsigned int *)values)[5] = one;
+        ((unsigned int *)values)[6] = zero;
+        ((unsigned int *)values)[7] = zero;
+        ((unsigned int *)values)[8] = zero;
+        ((unsigned int *)values)[9] = zero;
+        ((unsigned int *)values)[10] = one;
+        ((unsigned int *)values)[11] = zero;
+        ((unsigned int *)values)[12] = zero;
+        ((unsigned int *)values)[13] = zero;
+        ((unsigned int *)values)[14] = zero;
+        ((unsigned int *)values)[15] = one;
+    }
+}
+
 Coord3D &Matrix4D::GetXVector(Coord3D &out) const
 {
     out.x = values[0];
@@ -30,4 +60,28 @@ Coord3D &Matrix4D::GetTranslationVector(Coord3D &out) const
     out.y = values[7];
     out.z = values[11];
     return out;
+}
+
+Matrix4D &Matrix4D::SetIdentity()
+{
+    unsigned int zero = 0;
+    unsigned int one = 0x3f800000;
+
+    ((unsigned int *)values)[0] = one;
+    ((unsigned int *)values)[1] = zero;
+    ((unsigned int *)values)[2] = zero;
+    ((unsigned int *)values)[3] = zero;
+    ((unsigned int *)values)[4] = zero;
+    ((unsigned int *)values)[5] = one;
+    ((unsigned int *)values)[6] = zero;
+    ((unsigned int *)values)[7] = zero;
+    ((unsigned int *)values)[8] = zero;
+    ((unsigned int *)values)[9] = zero;
+    ((unsigned int *)values)[10] = one;
+    ((unsigned int *)values)[11] = zero;
+    ((unsigned int *)values)[12] = zero;
+    ((unsigned int *)values)[13] = zero;
+    ((unsigned int *)values)[14] = zero;
+    ((unsigned int *)values)[15] = one;
+    return *this;
 }
--- src/math/matrix4d.h
+++ src/math/matrix4d.h
@@ -4,10 +4,14 @@
 
 class Matrix4D {
 public:
+    Matrix4D();
+    Matrix4D(bool identity);
+
     Coord3D &GetXVector(Coord3D &out) const;
     Coord3D &GetYVector(Coord3D &out) const;
     Coord3D &GetZVector(Coord3D &out) const;
     Coord3D &GetTranslationVector(Coord3D &out) const;
+    Matrix4D &SetIdentity();
 
     float values[16];
 };
